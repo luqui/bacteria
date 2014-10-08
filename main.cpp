41b68a3ad7bdffc6c6a38f9a196a8ca9cd825ec5
Luke Palmer <luqui@google.com>
2014-10-07 17:49:00 -0700

    Cutie pie

--- main.cpp
+++ main.cpp
@@ -4,26 +4,64 @@
 #include <list>
 #include <cstdlib>
 
-class DNA { };
 
 class vec2 {
  public:
+  vec2(double x, double y) : x(x), y(y) { }
   double x, y;
+
+  vec2& operator *= (double a) {
+    x *= a;
+    y *= a;
+  }
+
+  vec2& operator += (const vec2& v) {
+    this->x += v.x;
+    this->y += v.y;
+  }
+
 };
+
+static vec2 operator* (double a, vec2 v) {
+  return v *= a;
+}
+
+static vec2 operator+ (vec2 u, const vec2& v) {
+  return u += v;
+}
+
+
+class RandomGen {
+ public:
+  double range(double min, double max) {
+    double r = std::rand() / (double)RAND_MAX;
+    return min + (max - min) * r;
+  }
+};
+
+
+class DNA { };
+
 
 class Organism {
  private:
   DNA dna;
   vec2 position;
+  RandomGen gen;
 
  public:
-  Organism(DNA dna, vec2 position) : dna(dna), position(position) { }
+  Organism(DNA dna, vec2 position, RandomGen gen)
+      : dna(dna), position(position), gen(gen)
+  { }
 
   double size() const {
     return 0.1;
   }
 
-  void step(double dt) { }
+  void step(double dt) {
+    position += dt * vec2(gen.range(-1,1), gen.range(-1,1));
+  }
+
   void draw() {
     glPushMatrix();
     glTranslated(position.x, position.y, 0);
@@ -45,6 +83,10 @@ class Simulation {
   std::list<Organism> organisms;
 
  public:
+  void add_organism(const Organism& org) {
+    organisms.push_back(org);
+  }
+
   void step(double dt) {
     for (std::list<Organism>::iterator i = organisms.begin(); i != organisms.end(); ++i) {
       i->step(dt);
@@ -80,7 +122,10 @@ int main() {
     return 1;
   }
 
+  glScaled(1/4.0, 1/3.0, 1);
+
   Simulation sim;
+  sim.add_organism(Organism(DNA(), vec2(0,0), RandomGen()));
 
   double dt = 1/30.0f;
   while(true) {
